f0f3143ce0a90edf56c6ecf8fc81b66f5f11191b
Steven Kraft <kraftydinosaur@gmail.com>
2017-06-22 02:14:18 -0400

    Added Polite form to Negative Verb Page

--- projects/japanese/negativeform/negativeform.js
+++ projects/japanese/negativeform/negativeform.js
@@ -1,17 +1,34 @@
-function getAnswer(word, type) {
-  var endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
-	stem = /(.*)(?!$)/.exec(word)[0];
+function getAnswer(word, type, polite=false) {
+  if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+  if (polite) {
+    var endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+    var ending = "ません"
+  } else {
+    var endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
+    var ending = "ない"
+  }
+  stem = /(.*)(?!$)/.exec(word)[0];
+
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-    if (word == "ある") {
+    if (word == "ある" && !polite) {
       $('#type').text("う-Verb (Godan) (Exception)");
       return "ない";
     }
-	  return stem + endings[lastchar] + "ない";
+	  return stem + endings[lastchar] + ending;
 	}
-	else if (type == "ru") {return stem + "ない";} //Ru-verbs
+	else if (type == "ru") {
+    return stem + ending;
+  } //Ru-verbs
 	else {
-		if (word == "する") {return "しない";}
-		else if (word == "くる") {return "こない";}
-	} // Irregular
+		if (word == "する") {return "し" + ending;}
+		else if (word == "くる") {
+      if (polite) {return "き" + ending;}
+      else {return "こ" + ending;}
+	   } // Irregular
+   }
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
