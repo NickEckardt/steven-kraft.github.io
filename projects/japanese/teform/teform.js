aecabb167a7d6e469d76f52dc61a636420625d0e
Steven Kraft <kraftydinosaur@gmail.com>
2017-06-23 00:59:35 -0400

    Added Negatives to Te-Form

--- projects/japanese/teform/teform.js
+++ projects/japanese/teform/teform.js
@@ -1,7 +1,14 @@
-function getAnswer(word, type) {
+function getAnswer(word, type, neg=false) {
+	if ($('#negsetting').is(':visible')) {neg = $('#negsetting').is(':checked')}
 	stem = /(.*)(?!$)/.exec(word)[0];
+	var endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
+
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
+		if (neg) {
+			neg_stem = stem + endings[lastchar] + "な"
+			return [neg_stem + "くて", neg_stem + "いで"]
+		}
 		if (['う', 'つ', 'る'].includes(lastchar)) {return stem + "って";}
 		else if (['む', 'ぶ', 'ぬ'].includes(lastchar)) {return stem + "んで";}
 		else if (['く'].includes(lastchar)) {
@@ -15,9 +22,20 @@ function getAnswer(word, type) {
 		else if (["す"].includes(lastchar)) {return stem + "して";}
 		else {return "Error";}
 	}
-	else if (type == "ru") {return stem + "て";} //Ru-verbs
+	else if (type == "ru") {
+		if (neg) {return [stem + "なくて", stem + "ないで"]}
+		return stem + "て";
+	} //Ru-verbs
 	else {
+		if (neg) {
+			if (word == "する") {return ["しなくて", "しないで"];}
+			else if (word == "くる") {return ["こなくて", "こないで"];}
+		}
 		if (word == "する") {return "して";}
 		else if (word == "くる") {return "きて";}
 	} // Irregular
 }
+
+$('#negsetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
