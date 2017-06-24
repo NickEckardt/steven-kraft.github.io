91f28394a4c49ab3febe54a04a7c68ef55adb895
Steven Kraft <kraftydinosaur@gmail.com>
2017-06-24 15:08:48 -0400

    Updated Imperative form page to support polite form

--- projects/japanese/imperativeform/imperativeform.js
+++ projects/japanese/imperativeform/imperativeform.js
@@ -1,16 +1,38 @@
-function getAnswer(word, type) {
+function getAnswer(word, type, polite=false) {
+  if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+
   var endings = {く:"け", す:"せ", う:"え", ぐ:"げ", ぶ:"べ", つ:"て", む:"め", ぬ:"ね", る:"れ"};
 	stem = /(.*)(?!$)/.exec(word)[0];
+  if (polite) {ending = "なさい";}
+  else {ending = "ろ"}
+
 	if(type == "u") {
-		lastchar = /(.$)/.exec(word)[0];
-	  return stem + endings[lastchar];
+    lastchar = /(.$)/.exec(word)[0];
+
+    if (polite) {
+      endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+      return stem + endings[lastchar] + ending;
+    }
+
+    return stem + endings[lastchar];
 	}
 	else if (type == "ru") {
-    if (word == "くれる") {return ["くれ", "くれろ"];}
-    return stem + "ろ";
+    if (word == "くれる") {
+      if (polite) {return "くれなさるな"}
+      $('#type').text("る-Verb (Ichidan) (Exception)");
+      return ["くれ", "くれろ"];
+    }
+    return stem + ending;
   } //Ru-verbs
 	else {
-		if (word == "する") {return "しろ";}
-		else if (word == "くる") {return "こい";}
+		if (word == "する") {return "し" + ending;}
+		else if (word == "くる") {
+      if (polite) {return "き" + ending;}
+      return "こい";
+    }
 	} // Irregular
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
