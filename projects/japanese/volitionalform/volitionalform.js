80d59212432f6e8b018aca594603ac2f85747416
Steven Kraft <kraftydinosaur@gmail.com>
2017-06-26 00:25:40 -0400

    Added Polite Form to Volitional Page

--- projects/japanese/volitionalform/volitionalform.js
+++ projects/japanese/volitionalform/volitionalform.js
@@ -1,13 +1,35 @@
-function getAnswer(word, type) {
-	var endings = {く:"こ", す:"そ", う:"お", ぐ:"ご", ぶ:"ぼ", つ:"と", む:"も", ぬ:"の", る:"ろ"};
+function getAnswer(word, type, polite=false) {
+	if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+
+	if (polite) {
+		var ending = "ましょう"
+		var endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+	}
+	else {
+		var ending = "う"
+		var endings = {く:"こ", す:"そ", う:"お", ぐ:"ご", ぶ:"ぼ", つ:"と", む:"も", ぬ:"の", る:"ろ"};
+	}
+
 	stem = /(.*)(?!$)/.exec(word)[0];
+
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-	  return stem + endings[lastchar] + "う";
+	  return stem + endings[lastchar] + ending;
 	}
-	else if (type == "ru") {return stem + "よう";} //Ru-verbs
+	else if (type == "ru") {
+		if (polite) {return stem + ending}
+		return stem + "よう";
+	} //Ru-verbs
 	else {
-		if (word == "する") {return "しよう";}
-		else if (word == "くる") {return "こよう";}
+		if (!polite) {
+			ending = "よう"
+			if (word == "くる") {return "こ" + ending;}
+		}
+		if (word == "する") {return "し" + ending;}
+		else if (word == "くる") {return "き" + ending;}
 	} // Irregular
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
