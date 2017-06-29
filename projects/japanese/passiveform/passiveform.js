9b80930a407d105eb7a28fd50fb0bf12dbef308b
Steven Kraft <kraftydinosaur@gmail.com>
2017-06-29 14:31:03 -0400

    Added Polite Setting to Passive form Page

--- projects/japanese/passiveform/passiveform.js
+++ projects/japanese/passiveform/passiveform.js
@@ -1,13 +1,20 @@
-function getAnswer(word, type) {
+function getAnswer(word, type, polite=false) {
+  if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+  if (polite) {var ending = "ます"}
+  else {var ending = "る"}
   var endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-	  return stem + endings[lastchar] + "れる";
+	  return stem + endings[lastchar] + "れ" + ending;
 	}
-	else if (type == "ru") {return stem + "られる";} //Ru-verbs
+	else if (type == "ru") {return stem + "られ" + ending;} //Ru-verbs
 	else {
-		if (word == "する") {return "される";}
-		else if (word == "くる") {return "こられる";}
+		if (word == "する") {return "され" + ending;}
+		else if (word == "くる") {return "こられ" + ending;}
 	} // Irregular
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
