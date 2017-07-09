b1dcbef09904f4645e8eb4a177b779853497f99d
Steven Kraft <kraftydinosaur@gmail.com>
2017-07-09 03:12:10 -0400

    Added Negative Option to Provisional Form page

--- projects/japanese/provisionalform/provisionalform.js
+++ projects/japanese/provisionalform/provisionalform.js
@@ -1,21 +1,40 @@
-function getAnswer(word, type, polite=false) {
+function getAnswer(word, type, polite=false, neg=false) {
 	if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
-	var endings = {く:"け", す:"せ", う:"え", ぐ:"げ", ぶ:"べ", つ:"て", む:"め", ぬ:"ね", る:"れ"};
-	var pol_endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+	if ($('#negsetting').is(':visible')) {neg = $('#negsetting').is(':checked')}
+
 	stem = /(.*)(?!$)/.exec(word)[0];
 	lastchar = /(.$)/.exec(word)[0];
-	if (polite) {
-		if (type=="ru") {return stem + "ますなら"}
-		else if (type=="irr") {
-			if (word == "する") {return "しますなら";}
-			else if (word == "くる") {return "きますなら";}
-		}
-		else {return stem + pol_endings[lastchar] + "ますなら";}
+
+	if (polite && neg) {ending = "ませんなら"}
+	else if (polite) {ending = "ますなら"}
+	else if (neg) {
+		ending = "なければ"
+		endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
 	}
-	return stem + endings[lastchar] + "ば";
+	else {
+		if (type == "u") {ending = "ば";}
+		else {ending = "れば";}
+		endings = {く:"け", す:"せ", う:"え", ぐ:"げ", ぶ:"べ", つ:"て", む:"め", ぬ:"ね", る:"れ"};
+	}
+
+	if (polite) {endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};}
+
+	if (type == "u") {return stem + endings[lastchar] + ending;}
+	else if (type == "ru") {return stem + ending;}
+	else {
+		if (neg && !polite && word == "くる") {return "こなければ";}
+		else if (polite || neg){
+			if (word == "する") {return "し" + ending;}
+			else if (word == "くる") {return "き" + ending;}
+		}
+		else {return stem + ending}
+	}
 }
 
-
 $('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
+
+$('#negsetting').change(function() {
   answer = getAnswer(word.kana, word.type, this.checked);
 });
