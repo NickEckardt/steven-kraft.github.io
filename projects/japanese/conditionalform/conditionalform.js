66066b3fade3204489da0da4104d89e6136bc950
Steven Kraft <kraftydinosaur@gmail.com>
2017-07-28 15:36:48 -0400

    Added polite option to conditional form page

--- projects/japanese/conditionalform/conditionalform.js
+++ projects/japanese/conditionalform/conditionalform.js
@@ -1,7 +1,11 @@
-function getAnswer(word, type) {
+function getAnswer(word, type, polite=false) {
+	var pol_endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+	if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
+		if (polite) {return stem + pol_endings[lastchar] + "ましたら"}
 		if (['う', 'つ', 'る'].includes(lastchar)) {return stem + "ったら";}
 		else if (['む', 'ぶ', 'ぬ'].includes(lastchar)) {return stem + "んだら";}
 		else if (['く'].includes(lastchar)) {
@@ -15,9 +19,20 @@ function getAnswer(word, type) {
 		else if (["す"].includes(lastchar)) {return stem + "したら";}
 		else {return "Error";}
 	}
-	else if (type == "ru") {return stem + "たら";} //Ru-verbs
+	else if (type == "ru") {
+		if (polite) {return stem + "ましたら"}
+		return stem + "たら";
+	} //Ru-verbs
 	else {
+		if (polite) {
+			if (word == "する") {return "しましたら";}
+			else if (word == "くる") {return "きましたら";}
+		}
 		if (word == "する") {return "したら";}
 		else if (word == "くる") {return "きたら";}
 	} // Irregular
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
