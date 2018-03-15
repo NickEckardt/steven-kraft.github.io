c619b0d19adc2d51e4ee8345ae94f0add521dd82
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-14 23:49:07 -0400

    Added Polite Option for Causitive Form

--- projects/japanese/causativeform/causativeform.js
+++ projects/japanese/causativeform/causativeform.js
@@ -1,24 +1,35 @@
-function getAnswer(word, type, short=false) {
-  if ($('#shortsetting').show()) {short = $('#shortsetting').is(':checked')}
+function getAnswer(word, type, short=false, polite=false) {
+  if ($('#shortsetting').is(':visible')) {short = $('#shortsetting').is(':checked')}
+  if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
   var endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-    if (short) {return stem + endings[lastchar] + "す";}
+    if (short && polite) {return stem + endings[lastchar] + "します";}
+    else if (short) {return stem + endings[lastchar] + "す";}
+    else if (polite) {return stem + endings[lastchar] + "せます";}
 	  else {return stem + endings[lastchar] + "せる";}
 	}
 	else if (type == "ru") {
+    if (short && polite) {return stem + "さします";}
     if (short) {return stem + "さす";}
+    else if (polite) {return stem + "させます";}
     else {return stem + "させる";}
-  } //Ru-verbs
+  }
 	else {
     var irrending = "せる";
-    if (short) {irrending = "す"}
+    if (short && polite) {irrending = "します";}
+    else if (short) {irrending = "す";}
+    else if (polite) {irrending = "せます";}
 		if (word == "する") {return "さ" + irrending;}
 		else if (word == "くる") {return "こさ" + irrending;}
-	} // Irregular
+	}
 }
 
 $('#shortsetting').change(function() {
-  answer = getAnswer(word.kana, word.type, this.checked);
+  answer = getAnswer(word.kana, word.type);
+});
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type);
 });
