0290cc1a3044c7c07cbb58e1ed5f83fe93590e19
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-16 12:27:55 -0400

    Added Negative Option for Conditional Form

--- projects/japanese/conditionalform/conditionalform.js
+++ projects/japanese/conditionalform/conditionalform.js
@@ -1,11 +1,15 @@
-function getAnswer(word, type, polite=false) {
+function getAnswer(word, type, polite=false, neg=false) {
 	var pol_endings = {く:"き", す:"し", う:"い", ぐ:"ぎ", ぶ:"び", つ:"ち", む:"み", ぬ:"に", る:"り"};
+	var neg_endings = {く:"か", す:"さ", う:"わ", ぐ:"が", ぶ:"ば", つ:"た", む:"ま", ぬ:"な", る:"ら"};
 	if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+	if ($('#negsetting').is(':visible')) {neg = $('#negsetting').is(':checked')}
 
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-		if (polite) {return stem + pol_endings[lastchar] + "ましたら"}
+		if (polite && neg) {return stem + pol_endings[lastchar] + "ませんでしたら";}
+		if (neg) {return stem + neg_endings[lastchar] + "なかったら";}
+		if (polite) {return stem + pol_endings[lastchar] + "ましたら";}
 		if (['う', 'つ', 'る'].includes(lastchar)) {return stem + "ったら";}
 		else if (['む', 'ぶ', 'ぬ'].includes(lastchar)) {return stem + "んだら";}
 		else if (['く'].includes(lastchar)) {
@@ -20,13 +24,23 @@ function getAnswer(word, type, polite=false) {
 		else {return "Error";}
 	}
 	else if (type == "ru") {
-		if (polite) {return stem + "ましたら"}
+		if (polite && neg) {return stem + "ませんでしたら";}
+		if (neg) {return stem + "なかったら";}
+		if (polite) {return stem + "ましたら";}
 		return stem + "たら";
 	} //Ru-verbs
 	else {
+		if (polite && neg) {
+			if (word == "する") {return "しませんでしたら";}
+			else if (word == "くる") {return "きませんでしたら";}
+		}
 		if (polite) {
 			if (word == "する") {return "しましたら";}
 			else if (word == "くる") {return "きましたら";}
+		}
+		if (neg) {
+			if (word == "する") {return "しなかったら";}
+			else if (word == "くる") {return "こなかったら";}
 		}
 		if (word == "する") {return "したら";}
 		else if (word == "くる") {return "きたら";}
@@ -34,5 +48,9 @@ function getAnswer(word, type, polite=false) {
 }
 
 $('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type, this.checked);
+});
+
+$('#negsetting').change(function() {
   answer = getAnswer(word.kana, word.type, this.checked);
 });
