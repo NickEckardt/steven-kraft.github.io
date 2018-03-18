9d6b1520f931aa8df438b434566c6ab03596887f
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-18 12:53:06 -0400

    Added Polite Option to Potential Form

--- projects/japanese/potentialform/potentialform.js
+++ projects/japanese/potentialform/potentialform.js
@@ -1,15 +1,21 @@
-var endings = {く:"け", す:"せ", う:"え", ぐ:"げ", ぶ:"べ", つ:"て", む:"め", ぬ:"ね", る:"れ"};
-var meaningPrefix = "can"
-
-function getAnswer(word, type) {
+function getAnswer(word, type, polite=false) {
+	if ($('#politesetting').is(':visible')) {polite = $('#politesetting').is(':checked')}
+	var endings = {く:"け", す:"せ", う:"え", ぐ:"げ", ぶ:"べ", つ:"て", む:"め", ぬ:"ね", る:"れ"};
+	var meaningPrefix = "can"
+	if (polite) {ending = "ます"}
+	else {ending = "る"}
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-	  return stem + endings[lastchar] + "る";
+	  return stem + endings[lastchar] + ending;
 	}
-	else if (type == "ru") {return stem + "られる";} //Ru-verbs
+	else if (type == "ru") {return stem + "られ" + ending;} //Ru-verbs
 	else {
-		if (word == "する") {return "できる";}
-		else if (word == "くる") {return "こられる";}
+		if (word == "する") {return "でき" + ending;}
+		else if (word == "くる") {return "こられ" + ending;}
 	} // Irregular
 }
+
+$('#politesetting').change(function() {
+  answer = getAnswer(word.kana, word.type);
+});
