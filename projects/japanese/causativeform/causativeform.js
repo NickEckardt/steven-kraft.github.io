96e83c3112cc58f600fcaa8a10a5ef6ee9b5cb40
Steven Kraft <steven.kraftjp@gmail.com>
2018-03-28 22:29:57 -0400

    Added passive option to causative form

--- projects/japanese/causativeform/causativeform.js
+++ projects/japanese/causativeform/causativeform.js
@@ -1,4 +1,4 @@
-function getAnswer(word, type, opts = {short:false, neg:false, polite:false}) {
+function getAnswer(word, type, opts = {short:false, neg:false, polite:false, passive:false}) {
   rand = $('#randomsetting').is(':visible')
 	if(rand && $('#randomsetting').is(':checked')) {
 		for (opt in opts) {opts[opt] = Math.random() >= 0.5;}
@@ -20,17 +20,32 @@ function getAnswer(word, type, opts = {short:false, neg:false, polite:false}) {
 	stem = /(.*)(?!$)/.exec(word)[0];
 	if(type == "u") {
 		lastchar = /(.$)/.exec(word)[0];
-    if (opts.short && opts.polite && opts.neg) {return stem + endings[lastchar] + "しません";}
-    else if (opts.short && opts.polite) {return stem + endings[lastchar] + "します";}
-    else if (opts.polite && opts.neg) {return stem + endings[lastchar] + "せません";}
-    else if (opts.short && opts.neg) {return stem + endings[lastchar] + "さない";}
-    else if (opts.short) {return stem + endings[lastchar] + "す";}
-    else if (opts.polite) {return stem + endings[lastchar] + "せます";}
-    else if (opts.neg) {return stem + endings[lastchar] + "せない";}
-	  else {return stem + endings[lastchar] + "せる";}
+    var presuff = opts.short ? "し" : "せ";
+    if (opts.passive) {presuff = "せられ";}
+    if (opts.passive && opts.short) {presuff = "され"}
+    var short_end = opts.passive ? "される" : "す";
+    var short_neg_end = opts.passive ? "されない" : "さない";
+
+    if (opts.short && opts.polite && opts.neg) {answer = stem + endings[lastchar] + presuff + "ません";}
+    else if (opts.short && opts.polite) {answer = stem + endings[lastchar] + presuff + "ます";}
+    else if (opts.polite && opts.neg) {answer = stem + endings[lastchar] + presuff + "ません";}
+    else if (opts.short && opts.neg) {answer = stem + endings[lastchar] + short_neg_end;}
+    else if (opts.short) {answer = stem + endings[lastchar] + short_end;}
+    else if (opts.polite) {answer = stem + endings[lastchar] + presuff + "ます";}
+    else if (opts.neg) {answer = stem + endings[lastchar] + presuff + "ない";}
+	  else {answer = stem + endings[lastchar] + presuff + "る";}
+
+    return answer;
 	}
 	else if (type == "ru") {
+    if (opts.passive && opts.short) {return ""}
     var root = stem + "さ";
+    if(opts.passive){
+      if (opts.polite && opts.neg) {return root + "せられません";}
+      else if (opts.polite) {return root + "せられます";}
+      else if (opts.neg) {return root + "せられない";}
+      else {return root + "せられる";}
+    }
     if (opts.short && opts.polite && opts.neg) {return root + "しません";}
     else if (opts.short && opts.polite) {return root + "します";}
     else if (opts.polite && opts.neg) {return root + "せません";}
@@ -41,7 +56,9 @@ function getAnswer(word, type, opts = {short:false, neg:false, polite:false}) {
     else {return root + "せる";}
   }
 	else {
+    if (opts.passive && opts.short) {return ""}
     var irrending = "せる";
+
     if (opts.short && opts.polite && opts.neg) {irrending = "しません";}
     else if (opts.short && opts.polite) {irrending = "します";}
     else if (opts.polite && opts.neg) {irrending = "せません";}
@@ -49,6 +66,14 @@ function getAnswer(word, type, opts = {short:false, neg:false, polite:false}) {
     else if (opts.short) {irrending = "す";}
     else if (opts.polite) {irrending = "せます";}
     else if (opts.neg) {irrending = "せない";}
+
+    if(opts.passive){
+      if (opts.polite && opts.neg) {irrending = "せられません";}
+      else if (opts.polite) {irrending = "せられます";}
+      else if (opts.neg) {irrending = "せられない";}
+      else {irrending = "せられる";}
+    }
+
 		if (word == "する") {return "さ" + irrending;}
 		else if (word == "くる") {return "こさ" + irrending;}
 	}
